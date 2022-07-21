06a202f7a856eb9d2da555b74c2988b6ccc9b445
David Vradenburg <david@davidnlaura.com>
2022-07-20 21:50:26 -0400

    added google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+// Multiple products (previously known as trackers)
+ReactGA.initialize([
+  {
+    trackingId: "G-QZGGQ31P9J",
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
